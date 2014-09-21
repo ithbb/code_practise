2048a8225b3a8f144cd3c14cb2b50412b140f6a7
ithbb <ithbb@126.com>
2014-09-21 22:19:25 +0900

    Update Binary_Tree_Inorder_Traversal_Iterative.java

--- Binary_Tree_Inorder_Traversal_Iterative.java
+++ Binary_Tree_Inorder_Traversal_Iterative.java
@@ -1,22 +1,31 @@
 
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
         LinkedList<Integer> list = new LinkedList<Integer>();
-        Stack<Integer> stack = new Stack<Integer>();
+        
+        if (root == null) {
+            return list;
+        }
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
         
         TreeNode node = root;
         while(node != null || !stack.isEmpty()) {
+            
+            // Find the most left child.
             while(node != null) {
                 stack.push(node);
                 node = node.left;
             }
             if (!stack.isEmpty()) {
+                //Add the node on the top.
                 node = stack.peek();
                 list.add(new Integer(node.val));
+                
+                // Back to the parent
                 stack.pop();
+                
+                // start go to the right child
                 node = node.right;
             }
         }
