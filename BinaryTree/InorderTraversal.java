5b8ab878e3669c0a7e41c4120122bef7fbc8dda7
ithbb <ithbb@126.com>
2015-09-27 22:52:30 +0800

    Create InorderTraversal.java

---
+++ BinaryTree/InorderTraversal.java
@@ -0,0 +1,68 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        if (root == null) {
+            return list;
+        }
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+/*        if (root.left != null) {
+            stack.push(root.left);
+        }*/
+        
+        TreeNode curNode = root;
+        
+        while (!stack.empty() || curNode != null) {
+            
+            
+            while (curNode != null) {
+                stack.push(curNode);
+                curNode = curNode.left;
+            }
+            
+            
+            
+            curNode = stack.pop();
+            list.add(curNode.val);
+            
+           
+            curNode = curNode.right;
+            
+
+        }
+        
+        return list;
+        
+        //Version2
+/*        if (root.left != null) {
+            list.addAll(inorderTraversal(root.left));
+        }
+        
+        list.add(root.val);
+        
+        if (root.right != null) {
+            list.addAll(inorderTraversal(root.right));
+        }
+        
+        return list;*/
+    }
+}
