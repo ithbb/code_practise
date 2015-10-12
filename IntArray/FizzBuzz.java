00e67443c98b0f9e513b251866c90c3d705bf86f
ithbb <ithbb@126.com>
2015-10-12 16:20:03 +0900

    Create FizzBuzz.java

---
+++ IntArray/FizzBuzz.java
@@ -0,0 +1,30 @@
+/*
+Fizz Buzz 
+Given number n. Print number from 1 to n. But:
+
+when number is divided by 3, print "fizz".
+when number is divided by 5, print "buzz".
+when number is divided by both 3 and 5, print "fizz buzz".
+*/
+
+class Solution {
+    /**
+     * param n: As description.
+     * return: A list of strings.
+     */
+    public ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                results.add("fizz buzz");
+            } else if (i % 5 == 0) {
+                results.add("buzz");
+            } else if (i % 3 == 0) {
+                results.add("fizz");
+            } else {
+                results.add(String.valueOf(i));
+            }
+        }
+        return results;
+    }
+}
