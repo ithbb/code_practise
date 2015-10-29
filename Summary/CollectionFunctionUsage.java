72a711df4cf0abf63d8b6d03ae0e9d89e38bcc5e
ithbb <ithbb@126.com>
2015-10-29 10:54:36 +0900

    Create CollectionFunctionUsage.java

---
+++ Summary/CollectionFunctionUsage.java
@@ -0,0 +1,76 @@
+// Functions summary for collections usage
+
+public class CollectionFunctionUsage {
+
+/*
+* char[] / int[] / int[][]
+*/
+char[] charArray;
+int[]  intArray;
+int[][] intMatrix;
+
+public void arrayActions() {
+ 
+}
+
+
+
+/*
+* String 
+*/
+String string1;
+public void StringActions() {
+ 
+}
+
+
+/*
+* ArrayList/ LinkedList
+*/
+LinkedList<String> linkedlist;
+ArrayList<String> stringArryList;
+List<String> stringList;
+public void ListActions() {
+ 
+}
+
+/*
+* HashMap / HashSet
+*/
+HashMap<Integer, String> hashmap;
+HashSet<String> hashset;
+public void hashActions() {
+}
+
+/*
+* Stack / Queue
+*/
+Stack<String> stack;
+Queue<String> queue;
+public void stackActions() {
+}
+
+public void queueActions() {
+}
+
+
+/*
+*  Binary Tree
+*/
+private class TreeNode {
+}
+
+/*
+* Graph
+*/
+private class UndirectedGraphNode {
+}
+
+private class DirectedGraphNode {
+}
+
+
+
+
+
+}
