1c724d06c052e7affa2b68ecb480695b8994ff01
ithbb <ithbb@126.com>
2016-06-08 23:45:18 +0800

    Update Mocha.java

--- Design/Decorator/Mocha.java
+++ Design/Decorator/Mocha.java
@@ -12,6 +12,14 @@ public class Mocha extends CondimentDecorator {
   }
   
   public double cost() {
-    return 0.2 + beverage.cost();
+    double cost = beverage.cost();
+    if(getSize() == Size.Tall) {
+      cost += 0.1;
+    } else if (getSize() == Size.Grande) {
+      cost += 0.15;
+    } else if (getSize() == Size.Venti) {
+      cost += 0.2;
+    }
+    return cost;
   }
 }
