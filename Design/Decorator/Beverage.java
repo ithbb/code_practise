c45b6b3e1244f6e43e2eb5cf007244879ddb9592
ithbb <ithbb@126.com>
2016-06-08 23:23:31 +0800

    Create Beverage.java

---
+++ Design/Decorator/Beverage.java
@@ -0,0 +1,10 @@
+
+public class Beverage {
+  String description = " Unknown Beverage";
+  
+  public String getDescription() {
+      return description;
+  }
+  
+  public abstract double cost();
+}
