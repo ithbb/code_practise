51670a388fbb6bf8e0b53df91fc488c0af59d54f
ithbb <ithbb@126.com>
2023-10-02 14:14:30 -0700

    Update LongestSubStringNoRepeat.java

--- String/LongestSubStringNoRepeat.java
+++ String/LongestSubStringNoRepeat.java
@@ -1,2 +1,18 @@
 Class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    Set<Character> charSet = new Set<Character>();
+    int maxLength = 0;
+    int start = 0, end = 0;
+    for (int end = 0; end <s.length();) {
+      if (!charSet.contians(s.charAt(end)) {
+         charSet.add(s.charAt(end);
+         end++;
+        maxLength = Math.max(maxLength, end - start);
+      } else {
+        charSet.remove(s.charAt(start));
+        start++;
+    }
+    return maxLength;
+  }    
 }
+
